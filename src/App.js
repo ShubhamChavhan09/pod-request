c23e893a451480d55933499b38503e76aeadf3ce
Shubham <shubh.chavhan00@gmail.com>
2021-08-29 17:13:10 +0530

    Added basic styling for the landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import sound from "./sound.svg";
+import Man from "./Man.webp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div className="logo">
+        <img src={sound} alt="Sound wave" />
+        <h1>pod</h1>
+      </div>
+      <div className="main">
+        <img className="man" src={Man} alt="singer" />
+      </div>
+      <div className="info">
+        <h1>
+          <span>PUBLISH YOUR PODCASTS </span>
+          <br />
+          EVERYWHERE.
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Upload you audio to pod with a single click. We'll <br />
+          then distribute your podcast to Spotify. Apple <br />
+          Poadcasts, Google Podcasts, Pocket casts and morel.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
